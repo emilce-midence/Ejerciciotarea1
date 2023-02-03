d35899d17eb293ed0d73995a12bb7aa997404d62
emilcemidence <emmc0798@gmail.com>
2023-02-02 20:16:24 -0600

    Segundo commit
    Segundo commit de la tarea

--- Ejercicio tarea 1/Ejercicio tarea 1/Ejerciciotarea1.Designer.cs
+++ Ejercicio tarea 1/Ejercicio tarea 1/Ejerciciotarea1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Ejercicio_tarea_1
 {
-    partial class Form1
+    partial class Ejerciciotarea1
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -29,13 +29,107 @@ namespace Ejercicio_tarea_1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.IngresoBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ResultadoBox1 = new System.Windows.Forms.TextBox();
+            this.Calcularbutton1 = new System.Windows.Forms.Button();
+            this.Vaciarbutton1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(165, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Determinar si un valor es par o impar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(71, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese un valor:";
+            // 
+            // IngresoBox1
+            // 
+            this.IngresoBox1.Location = new System.Drawing.Point(234, 96);
+            this.IngresoBox1.Name = "IngresoBox1";
+            this.IngresoBox1.Size = new System.Drawing.Size(120, 20);
+            this.IngresoBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(115, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Resultado:";
+            // 
+            // ResultadoBox1
+            // 
+            this.ResultadoBox1.Location = new System.Drawing.Point(234, 185);
+            this.ResultadoBox1.Name = "ResultadoBox1";
+            this.ResultadoBox1.Size = new System.Drawing.Size(170, 20);
+            this.ResultadoBox1.TabIndex = 4;
+            // 
+            // Calcularbutton1
+            // 
+            this.Calcularbutton1.Location = new System.Drawing.Point(249, 122);
+            this.Calcularbutton1.Name = "Calcularbutton1";
+            this.Calcularbutton1.Size = new System.Drawing.Size(75, 23);
+            this.Calcularbutton1.TabIndex = 5;
+            this.Calcularbutton1.Text = "Calcular";
+            this.Calcularbutton1.UseVisualStyleBackColor = true;
+            this.Calcularbutton1.Click += new System.EventHandler(this.Calcularbutton1_Click);
+            // 
+            // Vaciarbutton1
+            // 
+            this.Vaciarbutton1.Location = new System.Drawing.Point(249, 211);
+            this.Vaciarbutton1.Name = "Vaciarbutton1";
+            this.Vaciarbutton1.Size = new System.Drawing.Size(75, 23);
+            this.Vaciarbutton1.TabIndex = 6;
+            this.Vaciarbutton1.Text = "Vaciar";
+            this.Vaciarbutton1.UseVisualStyleBackColor = true;
+            this.Vaciarbutton1.Click += new System.EventHandler(this.Vaciarbutton1_Click);
+            // 
+            // Ejerciciotarea1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Vaciarbutton1);
+            this.Controls.Add(this.Calcularbutton1);
+            this.Controls.Add(this.ResultadoBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.IngresoBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Ejerciciotarea1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox IngresoBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ResultadoBox1;
+        private System.Windows.Forms.Button Calcularbutton1;
+        private System.Windows.Forms.Button Vaciarbutton1;
     }
 }
 
--- Ejercicio tarea 1/Ejercicio tarea 1/Ejerciciotarea1.cs
+++ Ejercicio tarea 1/Ejercicio tarea 1/Ejerciciotarea1.cs
@@ -10,11 +10,32 @@ using System.Windows.Forms;
 
 namespace Ejercicio_tarea_1
 {
-    public partial class Form1 : Form
+    public partial class Ejerciciotarea1 : Form
     {
-        public Form1()
+        public Ejerciciotarea1()
         {
             InitializeComponent();
+        }
+
+        private void Calcularbutton1_Click(object sender, EventArgs e)
+        {
+            int numero;
+            numero = int.Parse(IngresoBox1.Text);
+
+            if ( numero % 2 == 0)
+            {
+                ResultadoBox1.Text = "El numero " + numero  + " es par";
+            }
+            else
+            {
+                ResultadoBox1.Text = "El numero " + numero  + " es impar";
+            }
+        }
+
+        private void Vaciarbutton1_Click(object sender, EventArgs e)
+        {
+            IngresoBox1.Clear();
+            ResultadoBox1.Clear();
         }
     }
 }
